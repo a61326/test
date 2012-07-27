6aeeddc84f5c9669bd566cda3a1b2dc166b324e8
Fanny <a61326@yahoo.com.tw>
2012-07-27 11:47:50 +0800

    test

---
+++ Chap 02 HelloWorld/Chap 02 HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Chap 02 HelloWorld
+//
+//  Created by ucom Apple14 on 2012/2/14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chap 02 HelloWorld/Chap 02 HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chap 02 HelloWorld
+//
+//  Created by ucom Apple14 on 2012/2/14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
